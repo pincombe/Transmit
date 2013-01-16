486efabce630f13b104cbddef126c65b7a67e77a
Keith Pincombe <pincombe@gmail.com>
2013-01-16 02:13:28 +0000

    added key to constructor

--- src/Transmit/Client.php
+++ src/Transmit/Client.php
@@ -6,21 +6,22 @@ class ClientException extends \Exception {}
 
 class Client
 {
-	public $hostname = '';
-	public $headers = array();
+	public $hostname;
+	public $key;
 
 	const TIMEOUT = 2;
 
-	public function __construct($hostname, $headers = array())
+	public function __construct($hostname, $key)
 	{
 		$this->hostname = $hostname;
-		$this->headers = $headers;
+		$this->key = $key;
 	}
 
 	public function get($uri)
 	{
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->hostname . $uri);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(sprintf('Key: %s', $this->key)));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_TIMEOUT, self::TIMEOUT);
 
@@ -33,6 +34,7 @@ class Client
 	{
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->hostname . $uri);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(sprintf('Key: %s', $this->key)));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_TIMEOUT, self::TIMEOUT);
 
